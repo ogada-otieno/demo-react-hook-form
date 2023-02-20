012e705ab53555831cfc9e393ffd0657f7e3fb15
Rodgers Ogada <ogadarodgers@gmail.com>
2023-02-20 10:13:10 +0300

    validation, refactoring

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
 import React from "react";
 import "./styles.css";
+import { useForm } from "react-hook-form";
 
-export default function App() {
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    console.log({ ...register("email") });
+  };
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-control">
           <label>Email</label>
-          <input type="text" name="email" />
+          <input
+            type="text"
+            name="email"
+            {...register("email", {
+              required: "Email is required.",
+              pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+              message: "Email is not valid",
+            })}
+          />
+          {errors.email && (
+            <p className="errorMsg">{errors.email.message}</p>
+          )}
+          {errors.email?.type === "pattern" && (
+            <p className="errorMsg">Email is not valid.</p>
+          )}
         </div>
         <div className="form-control">
           <label>Password</label>
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            {...register("password", {
+              required: "Password is required.",
+              minLength: {
+                value: 6,
+                message: "Password must be atleast 6 characters.",
+              },
+            })}
+          />
+          {errors.password && (
+            <p className="errorMsg">{errors.password.message}</p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="errorMsg">
+              Password should be at-least 6 characters.
+            </p>
+          )}
         </div>
         <div className="form-control">
           <label></label>
@@ -21,3 +65,5 @@ export default function App() {
     </div>
   );
 }
+
+export default App;
